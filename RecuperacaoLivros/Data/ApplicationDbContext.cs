55c4bb9d2d9cf9485ab829373d0a31537566e21d
Pedro Augusto <maracujafrut@gmail.com>
2025-08-22 20:00:21 -0300

    codigo

---
+++ RecuperacaoLivros/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RecuperacaoLivros.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<RecuperacaoLivros.Models.Livro> Livros { get; set; }
+}
