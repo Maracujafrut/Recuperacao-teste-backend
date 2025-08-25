dca235e134fed2ba92ca0e9f638db89fb00b2c7d
Pedro Augusto <maracujafrut@gmail.com>
2025-08-24 21:59:17 -0300

    Update Program.cs

--- RecuperacaoLivros/Program.cs
+++ RecuperacaoLivros/Program.cs
@@ -7,7 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseMySql(
+builder.Configuration.GetConnectionString("DefaultConnection"),
+ServerVersion.AutoDetect(
+builder.Configuration.GetConnectionString("DefaultConnection")
+)
+
+)
+);
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
